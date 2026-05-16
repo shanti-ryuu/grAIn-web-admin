e30dea93bf6adf1df5f4d88b54627ca619de0c3a
pipinskie <princejinstephenmoya@gmail.com>
2026-05-16 22:44:57 +0800

    added missing css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
+import './globals.css'
 import { Providers } from '@/components/Providers'
 import { ToastContainer } from '@/components/ui/Toast'
 
